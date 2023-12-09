8ab9248f5ec9eb15ac1dfdbf27242f4d1ccce10a
miwanoff <marina.iva.noffa@gmail.com>
2023-12-09 12:21:29 +0200

    Add shuffle

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-
 let playedCards = [];
-
 
 let number = 0;
 let isGame = true;
@@ -14,5 +12,19 @@ const playedCardsField = document.getElementById("played_cards");
 const info = document.getElementById("info");
 const rel = document.getElementById("reload");
 
-
 info.innerHTML = "Take the card!";
+
+function shuffle(arr) {
+  let rand, temp;
+  for (let i = 0; i < arr.length; i++) {
+    rand = Math.floor(Math.random() * (i + 1));
+    temp = arr[rand];
+    arr[rand] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+shuffle(cards);
+
+cardsField.innerHTML = cards;
